c9ee38b07b900134bc4938043a7ba6df177e22e5
Rasil <rmsanthes2018@gmail.com>
2023-05-26 14:52:08 +0530

    sidebar icon

--- client/src/Containers/Sidebar/Sidebar.jsx
+++ client/src/Containers/Sidebar/Sidebar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Sidebar.css'
 import {Link} from 'react-router-dom'
+import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 const Sidebar = () => {
   return (
     <div className="__navbar">
-        Sidebar
-        <Link to="">Home</Link>
+        <Link to=""><TodayOutlinedIcon/>Today</Link>
+        <Link to=""><StarBorderOutlinedIcon/>Important</Link>
+        <Link to=""><CalendarMonthOutlinedIcon/>Upcoming</Link>
+        <Link to=""><DeleteOutlineOutlinedIcon/>Trash</Link>
     </div>
   )
 }
